f21ceba1af07ea2e90d601f6215c834202fe0155
cbeo <cbeok@protonmail.com>
2020-01-23 11:03:09 -0600

    respecting possible getters on state read access

--- State.hx
+++ State.hx
@@ -1,13 +1,17 @@
 package;
 
+import haxe.ds.Option;
+
 class ConcreteState<T> {
   var state:T;
 
   function updateViews() {
   }
 
-  public function fields(name:String):Any {
-    return Reflect.field(state,name);
+  public function fields(name:String):Option<Any> {
+    return if (Reflect.hasField(state,name) || Reflect.hasField(state,"get_"+name))
+      Some(Reflect.getProperty(state,name))
+      else None;
   }
 
   public function checkout(fn:T -> Void) {
